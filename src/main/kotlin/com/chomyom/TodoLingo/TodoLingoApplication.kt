bbc01a50279f30b6577de83f24c7cf6867a5a762
puppy1012 <puppy1012@naver.com>
2025-03-08 23:57:34 +0900

    git & github

---
+++ src/main/kotlin/com/chomyom/TodoLingo/TodoLingoApplication.kt
@@ -0,0 +1,11 @@
+package com.chomyom.TodoLingo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoLingoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoLingoApplication>(*args)
+}
---
+++ src/test/kotlin/com/chomyom/TodoLingo/TodoLingoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chomyom.TodoLingo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoLingoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
